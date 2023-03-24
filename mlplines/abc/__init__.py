ae0fa14c429f3b23f28c975db462a71cc7e97275
JHuardC <joehuard2711@gmail.com>
2023-03-24 13:32:23 +0000

    Remapped abc imports.

--- mlplines/abc/__init__.py
+++ mlplines/abc/__init__.py
@@ -4,9 +4,9 @@ Abstract and Base Classes for modelling pipelines and component handlers
 
 Created on: Thu 01 Sep 2022
 
-@author: Joe Huard
+@author: JHuardC
 """
-from topic_modelling_pipelines.pipelines.abc._abc import\
+from mlplines.abc._abc import\
     AbstractCheckModel,\
     CheckModelTrue,\
     BaseHandler,\
